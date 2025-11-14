3690bab2571683577b6f347b07fbc70a76b88060
saifuddinrakib <saifrakib1@gmail.com>
2025-11-14 15:50:00 +0600

    Add functional photo gallery with navigation and mobile support

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@
 
   // Nav Menu
   $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       var hash = this.hash;
       var target = $(hash);
       if (target.length) {
@@ -20,7 +20,7 @@
           $(this).closest('li').addClass('active');
         }
 
-        if (hash == '#header') {
+        if (hash === '#header') {
           $('#header').removeClass('header-top');
           $("section").removeClass('section-show');
           return;
@@ -140,12 +140,175 @@
         filter: $(this).data('filter')
       });
     });
-
   });
 
   // Initiate venobox (lightbox feature used in portofilo)
   $(document).ready(function() {
     $('.venobox').venobox();
+  });
+
+  // Photo Gallery Navigation
+  $(document).ready(function() {
+    var $galleryImage = $('#galleryImage');
+    if ($galleryImage.length === 0) {
+      return;
+    }
+
+    // Gallery Configuration - Easy to modify
+    var galleryConfig = {
+      images: [
+        {
+          src: 'assets/img/achievements/NRF.jpg',
+          alt: 'NRF Achievement',
+          title: 'National Robotics Foundation Competition'
+        },
+        {
+          src: 'assets/img/achievements/NRF_hackathon.jpg',
+          alt: 'NRF Hackathon Achievement',
+          title: 'NRF Hackathon Winner'
+        },
+        {
+          src: 'assets/img/achievements/Techfest.jpg',
+          alt: 'Techfest Achievement',
+          title: 'Techfest Competition Award'
+        }
+      ],
+      fadeSpeed: 200,
+      autoPlay: false,
+      autoPlayDelay: 5000
+    };
+
+    var currentIndex = 0;
+    var $dots = $('.gallery-dot');
+    var $prevBtn = $('.gallery-prev');
+    var $nextBtn = $('.gallery-next');
+    var isTransitioning = false;
+
+    function updateGallery(index) {
+      if (isTransitioning) return;
+
+      currentIndex = (index + galleryConfig.images.length) % galleryConfig.images.length;
+      isTransitioning = true;
+
+      var currentImage = galleryConfig.images[currentIndex];
+
+      $galleryImage.fadeOut(galleryConfig.fadeSpeed, function() {
+        $(this)
+          .attr('src', currentImage.src)
+          .attr('alt', currentImage.alt)
+          .attr('title', currentImage.title)
+          .fadeIn(galleryConfig.fadeSpeed, function() {
+            isTransitioning = false;
+          });
+      });
+
+      $dots.removeClass('active');
+      $dots.eq(currentIndex).addClass('active');
+    }
+
+    function preloadImages() {
+      galleryConfig.images.forEach(function(imageObj) {
+        var img = new Image();
+        img.src = imageObj.src;
+      });
+    }
+
+    // Initialize gallery
+    function initGallery() {
+      preloadImages();
+      $dots.eq(currentIndex).addClass('active');
+
+      // Set initial image attributes
+      var initialImage = galleryConfig.images[currentIndex];
+      $galleryImage
+        .attr('src', initialImage.src)
+        .attr('alt', initialImage.alt)
+        .attr('title', initialImage.title);
+    }
+
+    // Navigation button events
+    $prevBtn.on('click', function(e) {
+      e.preventDefault();
+      updateGallery(currentIndex - 1);
+    });
+
+    $nextBtn.on('click', function(e) {
+      e.preventDefault();
+      updateGallery(currentIndex + 1);
+    });
+
+    // Dot navigation
+    $dots.on('click', function(e) {
+      e.preventDefault();
+      var targetIndex = parseInt($(this).data('index'));
+      if (targetIndex !== currentIndex) {
+        updateGallery(targetIndex);
+      }
+    });
+
+    // Keyboard navigation
+    $(document).on('keydown', function(e) {
+      if ($galleryImage.is(':visible')) {
+        switch(e.keyCode) {
+          case 37: // Left arrow
+            e.preventDefault();
+            updateGallery(currentIndex - 1);
+            break;
+          case 39: // Right arrow
+            e.preventDefault();
+            updateGallery(currentIndex + 1);
+            break;
+          case 27: // Escape key (optional: could be used to close gallery if needed)
+            e.preventDefault();
+            break;
+        }
+      }
+    });
+
+    // Touch/swipe support for mobile
+    var touchStartX = 0;
+    var touchEndX = 0;
+
+    $galleryImage.on('touchstart', function(e) {
+      touchStartX = e.originalEvent.touches[0].clientX;
+    });
+
+    $galleryImage.on('touchend', function(e) {
+      touchEndX = e.originalEvent.changedTouches[0].clientX;
+      var diff = touchStartX - touchEndX;
+
+      if (Math.abs(diff) > 50) { // Minimum swipe distance
+        if (diff > 0) {
+          updateGallery(currentIndex + 1); // Swipe left - next image
+        } else {
+          updateGallery(currentIndex - 1); // Swipe right - previous image
+        }
+      }
+    });
+
+    // Auto-play functionality
+    var autoPlayInterval;
+
+    function startAutoPlay() {
+      if (galleryConfig.autoPlay) {
+        autoPlayInterval = setInterval(function() {
+          updateGallery(currentIndex + 1);
+        }, galleryConfig.autoPlayDelay);
+      }
+    }
+
+    function stopAutoPlay() {
+      clearInterval(autoPlayInterval);
+    }
+
+    // Pause auto-play on hover
+    if (galleryConfig.autoPlay) {
+      $('.gallery-container').hover(stopAutoPlay, startAutoPlay);
+    }
+
+    // Initialize the gallery
+    initGallery();
+    startAutoPlay();
   });
 
 })(jQuery);
